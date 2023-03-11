bf709ac62de106e0488e21cd64a320677c8821b8
archervista20 <archervista20@yahoo.com>
2023-03-11 20:31:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+// import ProjectPage from './components/ProjectPage/ProjectPage';
 import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero'
-// import Practice from './components/Practice';
+import Hero from './components/Hero/Hero';
+import Stacks from './components/Stacks/Stacks';
 // import {Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     // <Router>
     //    <Routes>
-    //     <Route></Route>
-    //    </Routes>
+    //     <Route></Route></Routes>
     // </Router>
     <div>
       <Header />
       <Hero />
-      {/* <Practice /> */}
+      <Stacks />
+      {/* <ProjectPage /> */}
     </div>
     
     
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,29 +3,24 @@ import { Link } from 'react-router-dom'
 import './header.css'
 
 const Header = () => {
-  
-  const [click, setClick] = useState(true)
-  const toggleMenu = () => {
-     setClick(!click)
-  }
+  const [click, setClick] = useState(false)
 
-  useEffect(() => {
-    setClick(true)
-  }, []);
+  const toggleMenuBtn = () => {
+    setClick(prevClick => prevClick = !click)
+  }
   
   return (
-    <header className="header">
-      <Link to='/'>Hello World</Link>
-      <nav className="nav-menu">
-        <ul className="links">
-          <li className="link"><Link to='./products'>Products</Link></li>
-          <li className="link"><Link to='./destinations'>Destinations</Link></li>
-          <li className="link"><Link to='./reservations'>Reservations</Link></li>
-          <li className="link"><Link to='./contact-us'>Contact Us</Link></li>
+    <header className="primary-header">
+      <Link to='/' className='logo'>ArcherVista</Link>
+      <nav className={click ? "navigation" : "navigation-inactive"}>
+        <ul className="nav-links">
+            <li><Link to='/profile'>PROFILE</Link></li>
+            <li><Link to='/projects'>PROJECTS</Link></li>
+            <li><Link to='/contact'>CONTACT</Link></li>
         </ul>
       </nav>
-      <div onClick={toggleMenu} className='menu'>
-        { click ? <span class="material-symbols-outlined">menu</span> : <span class="material-symbols-outlined">close</span>}
+      <div onClick={toggleMenuBtn} className="menu">
+        <i className={ click ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
     </header>
   )
